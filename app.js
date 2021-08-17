b9a40f7a01298e715b13f6510ca263a41ea7e1d4
zamiraraupova <87194673+zamiraraupova@users.noreply.github.com>
2021-08-17 12:42:41 -0400

    clicking the box in progress ... doesn't click yet

--- app.js
+++ app.js
@@ -1,131 +1,88 @@
-// const board = document.querySelector(".play-area");
-// const block = document.querySelectorAll(".block")
-// const display = document.querySeliector(".status")
+// connecting html to js
+const display = document.querySelector('.message');
 
 
-let playBoard = [
-    ["", "", ""],   //[0][0]
-    ["", "", ""],   //[1][1] 
-    ["", "", ""]    //[2][2]
-]
+let currentPlayer = "X"; // player X will start the game
+let board = ["", "", "", "", "", "", "", "", ""]; // array of the board to pick the index for clicks
 
+//display messages of who will win/tie/turn
+const winMsg = () => `Player ${currentPlayer} has won!`;
+const tieMsg = () => `It is a tie!`;
+const whoseTurn = () => `It's ${currentPlayer}'s turn`;
 
-const currentPlayer = "X";
-const computer = "O";
+display.innerHTML = whoseTurn();
 
-// 1 2 3
-// 4 5 6 
-// 7 8 9
+const whoWin = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-const whoWin = [         
-    [0,1,3],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-
-]
-
-function play(clickBlock, clickBlockIndex) {
-    playBoard[clickBlockIndex] = currentPlayer;
-    clickBlock.innerHTML = currentPlayer;
+function blockClicked(blck, blckIndex) {  //showing if the block was clicked and played by the current player
+    board[blckIndex] = currentPlayer;   // block index from 0 to 8
+    blck.innerHTML = currentPlayer;
 }
 
-function winner() {
-    let roundWon = false;
-    for (let i = 0; i < 8; i++) {
-        const winCond = whoWin[i];
-        let a = gameState[winCond[0]];
-        let b = gameState[winCond[1]];
-        let c = gameState[winCond[2]];
-        if (a === '' || b === '' || c === '') {
-            continue;
-        }
-        if (a === b && b === c) {
-            roundWon = true;
-            break
-        }
-    }
 
-    if (roundWon) {
-        display.innerHTML = winningMessage();
-        gameActive = false;
-        return;
-    }
+
+
+
+function changePlayer() {              // strictly eaqul to X 
+     if(currentPlayer === "X") {       // if equal to O / else X
+        currentPlayer === "O"
+    } else {
+        currentPlayer === "X"
+    }  
+    display.innerHTML = whoseTurn();
+}
+console.log(changePlayer)
 
 
 
 
 
 
+
+
+
+
+// function equals3(a,b,c){
+//   return (a==b && b==c && a != "");
+// }
 //     function winner(){
 //     let winner = null;
 // //rows
 //     for(let i = 0; i < 3; i++) {
-//         if(playBoard[i][0] == playBoard[i][1] == playBoard[i][2]) {
+//         if(equals3(playBoard[i][0],playBoard[i][1],playBoard[i][2])) {
 //            winner = playBoard[i][0];     
 //         }
 //     }
 //     //colums
 //     for(let i = 0; i < 3; i++) {
-//         if(playBoard[0][i] == playBoard[0][i] == playBoard[0][i]) {
+//         if(equals3(playBoard[0][i],playBoard[0][i],playBoard[0][i])) {
 //            winner = playBoard[0][i];     
 //         }
 //     }
 //     //dioganals
-//         if(playBoard[0][0] == playBoard[1][1] == playBoard[2][2]) {
+//         if(equals3(playBoard[0][0],playBoard[1][1],playBoard[2][2])) {
 //            winner = playBoard[0][0];   //??  
 //         }
         
-//         if(playBoard[0][2] == playBoard[1][1] == playBoard[2][0]) {
+//         if(equals3(playBoard[0][2],playBoard[1][1],playBoard[2][0])) {
 //             winner = playBoard[2][0];  // ??   
 //         }
 //     if (winner == null) {
-//         console.log("It is a tie");
+//         return "It is a tie";
 //       } 
 //       else{
-//           console.log(winner)
+//           return winner
 //       } 
 // }
-
-// let currentPlayer;
-// function play() {
-//     if (random(1) < 0.5) {
-//         currentPlayer = player;
-//     }
-//     else{
-//         currentPlayer = computer
-//     }
-// }
-
-// for (let i = 0; i < 3; i++){
-//     if ()
-// }
-
-
-
-
-
-
-
-// // reset();
-
-// // function reset() {
-// //     document.querySelector(".endGame").style.display = "none"
-// //     playBoard = Array.from(Array(9).keys());
-// //     console.log(playBoard)
-// // }
-
-
-
-
-
-
-
-
 
 
 
