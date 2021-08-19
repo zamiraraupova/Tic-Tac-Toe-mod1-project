1d2b77c48e938dc13f03080ddebdbb75cbe1e9ef
zamiraraupova <87194673+zamiraraupova@users.noreply.github.com>
2021-08-18 21:29:36 -0400

    added the working click

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 // connecting html to js
 const display = document.querySelector('.message'); //will show the message whose turn is and who will win/lose
-document.querySelectorAll('.block').forEach(block => block.addEventListener('click', blockClick));
-document.querySelector('.restart').addEventListener('click', restart);
-
-let playGame = true; // this is needed for pause if game ended
-let currentPlayer = "X"; // player X will start the game
 let board = ["", "", "", "", "", "", "", "", ""]; // array of the board to pick the index for clicks
 
-const whoWin = [      
-    [0, 1, 2],        // [1 2 3]
-    [3, 4, 5],        // [4 5 6] 
-    [6, 7, 8],        // [7 8 9] 
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+let playGame = true; // this is needed for pause if game ended //if game is still active
+let currentPlayer = "X"; // player X will start the game
+
+document.querySelectorAll('.block').forEach((block, index) => block.addEventListener('click', () => blockPlayed(block,index)));
+document.querySelector('.restart').addEventListener('click', restart);
+
+const whoWin = [
+  [0, 1, 2],        // [1 2 3]
+  [3, 4, 5],        // [4 5 6] 
+  [6, 7, 8],        // [7 8 9] 
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
 
 function blockPlayed(clickedBlock, clickedBlockIndex) {
-    board[clickedBlockIndex] = currentPlayer;
-    clickedBlock.innerHTML = currentPlayer;
+  console.log(clickedBlock, clickedBlockIndex)
+  board[clickedBlockIndex] = currentPlayer;
+  clickedBlock.innerHTML = currentPlayer;
+  changePlayer()
 }
+
 
 function changePlayer() {
   currentPlayer = currentPlayer === "X" ? "O" : "X";
@@ -29,41 +33,56 @@ function changePlayer() {
 }
 
 function results() {
-    let winner = false;
-    for (let i = 0; i <= 7; i++) {
-        const win = whoWin[i];
-        let a = board[win[0]];
-        let b = board[win[1]];
-        let c = board[win[2]];
-        
-        if (a === '' || b === '' || c === '') {
-            continue;
-        }
-        if (a === b && b === c) {
-            winner = true;
-            break
-        }
-    }
+  let winner = false;
+  for (let i = 0; i <= 7; i++) {
+    const win = whoWin[i];
+    let a = board[win[0]];
+    let b = board[win[1]];
+    let c = board[win[2]];
 
-    if (winner) {
-        display.innerHTML = `Player ${currentPlayer} has won!`;
-        playGame = false;
-        return;
+    if (a === '' || b === '' || c === '') {
+      continue;
     }
-
-    let tie = !board.includes("");
-    if (tie) {
-        display.innerHTML = `It is a tie!`;
-        playGame = false;
-        return;
+    if (a === b && b === c) {
+      winner = true;
+      break
     }
+  }
 
-    changePlayer();
+  if (winner) {
+    display.innerHTML = `Player ${currentPlayer} has won!`;
+    playGame = false;
+    return;
+  }
+
+  let tie = !board.includes("");
+  if (tie) {
+    display.innerHTML = `It is a tie!`;
+    playGame = false;
+    return;
+  }
+
+  changePlayer();
+}
+
+
+
+function restart() {
+  playGame = true;
+  board = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "X";
+  display.innerHTML = `It's ${currentPlayer}'s turn`;
+  document.querySelectorAll('.block').forEach(block => block.innerHTML = "");
 }
 
 
 
 
+// const board = [
+//   ["","",""],
+//   ["","",""],
+//   ["","",""]
+// ]
 
 // function equals3(a,b,c){
 //   return (a==b && b==c && a != "");
@@ -86,7 +105,7 @@ function results() {
 //         if(equals3(board[0][0],board[1][1],board[2][2])) {
 //            winner = board[0][0];   //??  
 //         }
-        
+
 //         if(equals3(board[0][2],board[1][1],board[2][0])) {
 //             winner = board[2][0];  // ??   
 //         }
